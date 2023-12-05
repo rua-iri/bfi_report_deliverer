e28d2daa7447dc60d9e252445ea0332db76239ef
rua-iri <rory.mcguigan@protonmail.com>
2023-12-05 11:43:58 +0300

    update: implement file downloading

--- main.py
+++ main.py
@@ -1,24 +1,43 @@
+import logging
 import requests
 from bs4 import BeautifulSoup
+
+logger = logging.getLogger(__name__)
+logger.setLevel(level=logging.DEBUG)
 
 URL = "https://www.bfi.org.uk/industry-data-insights/weekend-box-office-figures"
 
 
 def main():
-    find_latest_file()
+    is_download_successful = find_latest_file()
+
+    if is_download_successful:
+        pass
 
 
 def find_latest_file():
     response = requests.get(url=URL)
     soup = BeautifulSoup(response.text, "html.parser")
-    
-    latestFileLink = soup.find('a', {'class': 'FileDownload__Link-sc-ix3u4x-1'})
-    print(latestFileLink.get('href'))
+    latestFileLink = soup.find("a", {"class": "FileDownload__Link-sc-ix3u4x-1"})
 
-def download_latest_file(file_path: str):
-    pass
+    return download_latest_file(download_url=latestFileLink.get("href"))
 
 
+def download_latest_file(download_url: str):
+    print(download_url)
 
-if __name__=="__main__":
+    try:
+        response = requests.get(url=download_url)
+
+        with open(file="downloads/latest_report.xlsx", mode="wb") as spreadsheet_file:
+            spreadsheet_file.write(response.content)
+
+        return True
+
+    except Exception as e:
+        logger.error("Error: ", exc_info=True)
+        return False
+
+
+if __name__ == "__main__":
     main()
